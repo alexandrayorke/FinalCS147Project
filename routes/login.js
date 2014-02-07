e8c69c8f16fe6e1abd5108b573ecf7eb7b2c589b
vagrant <vagrant@precise32.(none)>
2014-02-07 04:41:01 +0000

    working

--- routes/login.js
+++ routes/login.js
@@ -1,4 +1,12 @@
-exports.login = function(req, res) { 
-	res.render('index');   
-	// Your code goes here
- }
+exports.loginFunction = function(req, res) {
+	//var username = req.query.username;
+	//var password = req.query.password;
+	//if (username == null || password == null) {
+		//$("#loginInstructions").text("Please enter a username and password.");
+	//} else {
+	//	res.render('homepage');
+		//window.location.href = ("/views/homepage.handlebars");
+	//}
+	res.render('homepage');
+}
+
