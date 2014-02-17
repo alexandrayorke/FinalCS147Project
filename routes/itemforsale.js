dd8269e218a971eea13933c972cfa80395c1015b
vagrant <vagrant@precise32.(none)>
2014-02-17 22:57:45 +0000

    adding name to product

--- routes/itemforsale.js
+++ routes/itemforsale.js
@@ -5,24 +5,31 @@ exports.view = function(req, res) { 
 	var price = req.query.price;
 	var image = req.query.imageUrl;
 	var category = req.query.category;
+	var sellerEmail = req.session.user["email"];
+	var sellerName = req.session.user["firstName"] + " " + req.session.user["lastName"];
+	console.log("seller" + sellerEmail);
+	console.log("sellername"+ sellerName);
 	console.log("desc:" + desc);
 	console.log("cat:" + category);
-	//var dataSz= data["items"].length;
+	
 
 	var newItem = {
 	 	"image": image,
 	  	"description": desc,
 	  	"price": price,	
 	  	"category": category,
+	  	"sellerEmail": sellerEmail,
+	  	"sellerName": sellerName,
 	  	"id": req.session.nextID	
 	  };
 
 	req.session.nextID = req.session.nextID + 1;	
-	console.log("new item:" + newItem);
+	var newNotification= "Put " + desc + " up for sale";
+	console.log("new notificaiton" + newNotification);
 	console.log("new item title:" + newItem.description);
-	console.log("new item price:" + newItem.price);
-	console.log("new item cat:" + newItem.category);
+	
 	data["items"].push(newItem); 
+	//data["users"][sellerEmail]["notifications"].push(newNotification);
 	var pageInfo = {'user': req.session.user, 'data': data, 'nextID': req.session.nextID};
 	res.render('homepage', pageInfo);
 
